a9f8087916c5b70a9580034721c6f8fcc92996ed
Jagat Sastry Pudipeddi <jagatsastry@gmail.com>
2013-12-01 01:11:37 -0500

    elf is parsing and sthe hello program with libc is working.

--- sys/kb.c
+++ sys/kb.c
@@ -11,10 +11,10 @@ void keyboard_handler(struct regs *r)
     static uint08_t SHIFT_PRESSED = 0;
 
     unsigned char scancode;
-
     scancode = port_inb(0x60);
-
-
+    //int *keyPressedPtr = (int *)r->r10;
+    //printf("Valus of KeyPressed is %p", keyPressedPtr); 
+    //printf("Scancode is %d",scancode);
     if (scancode & 0x80)
     {
         if(scancode - 0x80 == SHIFT_SCANCODE)
@@ -27,6 +27,18 @@ void keyboard_handler(struct regs *r)
         else 
         {
             unsigned char pressedKey = kbdus[scancode];
+            if( scancode == 28){
+               printf("\nEntered Pressed");
+               /*if( enterPressed == 0){
+                     buff[ curBuffIndex ] = pressedKey;
+                     curBuffIndex++;
+               }*/ 
+               *keyPressedPtr = 1; 
+               // while(1); 
+               //printf("Value of keyPressed %d",*keyPressed); 
+               enterPressed = 1;
+               return; 
+            } 
             if (SHIFT_PRESSED == 1) {
 		    if(scancode >= 2 && scancode <= 13)
 			    pressedKey = shiftNums[scancode - 2];
@@ -44,9 +56,16 @@ void keyboard_handler(struct regs *r)
                 }
             }
 	    }
-        
-            print_key(pressedKey);
-        }
+            if( enterPressed == 0 ){
+                buff[curBuffIndex ] = pressedKey; 
+                curBuffIndex++; 
+                putc( pressedKey ); 
+                //print_key(pressedKey); 
+            }else{
+                putc( pressedKey ); 
+                //print_key(pressedKey);
+            } 
+           }
     }
 }
 
